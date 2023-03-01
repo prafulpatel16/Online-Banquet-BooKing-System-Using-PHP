83baf985fcc261a68957c5ee23976f126280a669
prafulpatel16 <praful1611@outlook.com>
2023-03-01 12:37:14 -0600

    OBBS system

--- obbs/includes/dbconnection.php
+++ obbs/includes/dbconnection.php
@@ -1,8 +1,8 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
+define('DB_HOST','mysql');
 define('DB_USER','root');
-define('DB_PASS','');
+define('DB_PASS','root');
 define('DB_NAME','obbs');
 // Establish database connection.
 try
